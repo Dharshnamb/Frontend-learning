a9deca13229dbfd2b42c3cd01665fc41a49ef313
Dharshnamb <dharshnamb@gmail.com>
2025-11-01 22:01:32 +0530

    Add files via upload

---
+++ JAVASCRIPT/MY PRACTICE/demo.js
@@ -0,0 +1,143 @@
+/*
+function samsung()
+{
+    console.log("this is samsung phone")
+}
+samsung()
+function iphone()
+{
+    console.log("this is apple iphone")
+}
+iphone()
+function redmi()
+{
+    console.log("this is redmiphone")
+}
+redmi()
+*/
+
+
+/*  
+var a=10
+var b=20
+function add(){
+    console.log(a+b)
+}
+add()
+*/
+
+/*
+var factor = "Suriya"
+var fplayer = "Dhoni"
+var fmovie = "Soorarai potru"
+function favourite(){
+    console.log("Favourite Actor:",factor)
+    console.log("Favourite Player:",fplayer)
+    console.log("Favourite Movie:",fmovie)
+}
+favourite()
+*/
+
+/*
+function addi(a,b)
+{
+   console.log(a+b) 
+}
+addi(10,20)
+*/
+
+/*
+function area(l,b){
+    var a=l*b
+    console.log("Area is:",a)
+}
+
+var length = 10
+var breadth = 20
+area(length,breadth)
+*/
+
+/*
+function myname(){
+    return "Dharshna"
+}
+var a = myname()
+console.log(a)
+*/
+
+/*
+var rain=false
+
+if(rain){
+    console.log("Take an umbrella")
+}
+else{
+    console.log("Enjoy the sunshine")
+}
+    */
+
+/*
+console.log(true&&true) //true
+console.log(false&&true) //false
+console.log(false&&false) //false
+console.log(true||true) //true
+console.log(false||true) //true
+console.log(false||false) //false
+console.log(!true) //false
+console.log(!false) //true
+*/
+
+/*
+var season="spring"
+if(season=="spring"){
+    console.log("Enjoy the blooming flowers")
+}
+if(season=="summer"){
+    console.log("Have fun in the sun")
+}
+if(season=="autumn"){
+    console.log("Admire the colorful leaves")
+}
+if(season=="winter"){
+    console.log("Bundle up and stay warm")
+}
+*/
+
+/*
+var score=70
+if(score<=50){
+    console.log("You need to improve")
+}
+else if(score>50&&score<=70){
+    console.log("Good job")
+}
+else(score>70){
+    console.log("excellent")
+}
+*/
+
+/*
+for(count=1;count<=5;count++){
+    console.log(count)
+}
+*/
+
+/*
+for(count=5;count>=1;count--){
+    console.log(count)
+}
+*/
+
+/*
+for(no=1;no<=10;no++){
+    if(no%2==0){
+        console.log(no)
+    }
+}
+*/
+
+/*
+for(n=1;n<=10;n++){
+    console.log(n+"*2="+(2*n))
+}
+*/ 
---
+++ JAVASCRIPT/MY PRACTICE/todo.js
@@ -0,0 +1,4 @@
+var fruit=["Apple","Orange","Grape","Mango","Banana"]
+for(count=0;count<5;count++){
+    console.log(fruit[count])
+}
